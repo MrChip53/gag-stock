40446601796e65c0c60450cc62b565cf645c9f4c
Mr. Chip53 <support@freebrew.org>
2025-07-30 00:18:02 -0500

    windows

---
+++ run-gag-stock.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Starting GAG Stock application...
+echo.
+
+REM Run the gag-stock.exe program
+gag-stock.exe
+
+echo.
+echo Program finished. Press any key to close this window...
+pause >nul 
